0750efd3059091989137dbbad929e72170263930
naschwertner <nathalia_13@outlook.com>
2023-11-25 14:59:29 -0300

    corrige internacionalizacao home e adiciona calendario

--- app/src/main/java/com/example/gs2semandrenathalia/View/AlertasFragment.kt
+++ app/src/main/java/com/example/gs2semandrenathalia/View/AlertasFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gs2semandrenathalia.View
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.gs2semandrenathalia.ViewModel.AlertasViewModel
 import com.example.gs2semandrenathalia.databinding.FragmentAlertasBinding
+import java.util.*
 
 class AlertasFragment : Fragment() {
 
@@ -22,12 +24,24 @@ class AlertasFragment : Fragment() {
         _binding = FragmentAlertasBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-
         val alertTitle = binding.tvAlertTitle
         val enableNotifications = binding.cbEnableNotifications
+        val etMaintenanceDate = binding.etMaintenanceDate
 
         slideshowViewModel.text.observe(viewLifecycleOwner) {
             alertTitle.text = it
+        }
+
+        etMaintenanceDate.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            DatePickerDialog(requireContext(), { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                val selectedDate = "${selectedDayOfMonth}/${selectedMonth + 1}/$selectedYear"
+                etMaintenanceDate.setText(selectedDate)
+            }, year, month, day).show()
         }
 
         return root
