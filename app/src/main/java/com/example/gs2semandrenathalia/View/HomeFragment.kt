992132820721a38eea626d9c3bf111447e1d4b52
naschwertner <nathalia_13@outlook.com>
2023-11-25 13:33:16 -0300

    corrige arquivos para internacionalização e adiciiona redirect na pagina inicial

--- app/src/main/java/com/example/gs2semandrenathalia/View/HomeFragment.kt
+++ app/src/main/java/com/example/gs2semandrenathalia/View/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.gs2semandrenathalia.R
 import com.example.gs2semandrenathalia.ViewModel.HomeViewModel
 import com.example.gs2semandrenathalia.databinding.FragmentHomeBinding
 
@@ -13,23 +15,28 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.btnSensorDetails.setOnClickListener { goToSensorDetailsFragment() }
+        binding.btnAlerts.setOnClickListener { goToAlertsFragment() }
 
         return root
+    }
+
+    private fun goToSensorDetailsFragment() {
+        findNavController().navigate(R.id.action_homeFragment_to_sensorFragment)
+    }
+
+    private fun goToAlertsFragment() {
+        findNavController().navigate(R.id.action_homeFragment_to_alertasFragment)
     }
 
     override fun onDestroyView() {
